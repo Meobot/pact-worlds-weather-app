9c28572ff08c35d075b618db835b3588d347bb6d
Meobot <nathanrhodes312@gmail.com>
2023-10-16 16:15:04 -0400

    temp added

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,13 +1,16 @@
 import { useState } from 'react'
 import './App.css'
 import Header from './components/Header'
+import Temperature from './components/Temperature'
 
 function App() {
-  const [count, setCount] = useState(0)
+
+
 
   return (
 	<div className="App">
 		<Header />
+		<Temperature />
 	</div>
   )
 }
