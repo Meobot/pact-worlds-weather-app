14ec09ae76166de4781048fc9ad63395b56df8d8
Meobot <nathanrhodes312@gmail.com>
2023-10-16 16:15:47 -0400

    component creation

---
+++ client/src/components/Temperature.tsx
@@ -0,0 +1,14 @@
+function Temperature() {
+
+	const temperature = 75
+
+  return (
+	<div className="Temperature-container">
+		<h2>
+			<p>Temperature: {temperature}&deg;F</p>
+		</h2>
+	</div>
+  )
+}
+
+export default Temperature
