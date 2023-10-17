2c8df3c040b441c499b12b3809013f0807734006
Meobot <nathanrhodes312@gmail.com>
2023-10-17 09:44:27 -0400

    initial stylings and layout

--- client/src/components/ForecastCard.tsx
+++ client/src/components/ForecastCard.tsx
@@ -1,11 +1,18 @@
-import React from 'react'
+import "../styles/ForecastCard.css";
+import { faCloud } from "@fortawesome/free-solid-svg-icons";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 
 function ForecastCard() {
-  return (
-	<div>
-		
-	</div>
-  )
+	return (
+		<div className="ForecastCard-container">
+			<FontAwesomeIcon icon={faCloud} style={{ color: "#ffffff" }} />
+			<div>
+				<h2>Today</h2>
+				<h2>Clear Skies</h2>
+			</div>
+			<h3>70&deg;F - 85&deg;F</h3>
+		</div>
+	);
 }
 
-export default ForecastCard
+export default ForecastCard;
