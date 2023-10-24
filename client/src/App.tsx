224ea43d04d7c27ce321abdfd8a73d9190669741
Meobot <nathanrhodes312@gmail.com>
2023-10-23 22:34:18 -0400

    test call to db implemented

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,24 +1,35 @@
-import './App.css'
-import Header from './components/Header'
-import CurrentWeather from './components/CurrentWeather'
-import ForecastCard from './components/ForecastCard'
+import "./App.css";
+import Header from "./components/Header";
+import CurrentWeather from "./components/CurrentWeather";
+import ForecastCard from "./components/ForecastCard";
 
 function App() {
+	fetch("http://localhost:3000/api/weather", {
+		headers: {
+			"Content-Type": "application/json",
+		},
+	})
+		.then((response) => response.json())
+		.then((data) => {
+			// Handle the weather data received from the server
+			console.log(data);
+		})
+		.catch((error) => {
+			console.error("Error:", error);
+		});
 
-
-
-  return (
-	<div className="App">
-		<Header />
-		<CurrentWeather />
-		<ForecastCard />
-		<ForecastCard />
-		<ForecastCard />
-		<ForecastCard />
-		<ForecastCard />
-		<ForecastCard />
-	</div>
-  )
+	return (
+		<div className="App">
+			<Header />
+			<CurrentWeather />
+			<ForecastCard />
+			<ForecastCard />
+			<ForecastCard />
+			<ForecastCard />
+			<ForecastCard />
+			<ForecastCard />
+		</div>
+	);
 }
 
-export default App
+export default App;
